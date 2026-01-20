f7a93e5b778861f40fe7f21be1574bd0a0599126
Marius Klepp <Mariusklepp@icloud.com>
2026-01-20 13:59:53 +0100

    small changes in structure

--- src/main/java/edu/ntnu/IDATT2003/marius/command/wrap/WrapTextCommand.java
+++ src/main/java/edu/ntnu/IDATT2003/marius/command/wrap/WrapTextCommand.java
@@ -1,4 +1,25 @@
 package edu.ntnu.idatt2003.marius.command.wrap;
+import edu.ntnu.idatt2003.marius.TextCommand;
 
-public class WrapTextCommand {
+public class WrapTextCommand implements TextCommand {
+    private final String opening;
+    private final String end;
+
+    public WrapTextCommand(String opening, String end) {
+        this.opening = opening;
+        this.end = end;
+    }
+
+    @Override
+    public String execute(String text) {
+        return opening + text + end;
+    }
+
+    public String getOpening() {
+        return opening;
+    }
+
+    public String getEnd() {
+        return end;
+    }
 }
--- src/test/java/edu/ntnu/idatt2003/marius/WrapTextCommandTest.java
+++ src/test/java/edu/ntnu/idatt2003/marius/WrapTextCommandTest.java
@@ -1,4 +1,36 @@
 package edu.ntnu.idatt2003.marius;
 
+import edu.ntnu.idatt2003.marius.command.wrap.WrapTextCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WrapTextCommandTest {
+    WrapTextCommand wrapCommand;
+
+    @BeforeEach
+    void setUp() {
+        wrapCommand = new WrapTextCommand("--", "==");
+    }
+
+    @Test
+    void executeTest() {
+        assertEquals("--Hello==",wrapCommand.execute("Hello"));
+    }
+
+    @Test
+    void executeWhenTextIsEmptyTest() {
+        assertEquals("--==",wrapCommand.execute(""));
+    }
+
+    @Test
+    void getOpeningTest() {
+        assertEquals("--", wrapCommand.getOpening());
+    }
+
+    @Test
+    void getEndTest() {
+        assertEquals("==", wrapCommand.getEnd());
+    }
 }
