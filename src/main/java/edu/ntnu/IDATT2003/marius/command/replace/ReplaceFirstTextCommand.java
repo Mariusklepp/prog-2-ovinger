7ebcfc42132ef5a62d3ceb360ab764383197d8af
Marius Klepp <Mariusklepp@icloud.com>
2026-01-20 13:08:55 +0100

    Made a replace package

---
+++ src/main/java/edu/ntnu/IDATT2003/marius/command/replace/ReplaceFirstTextCommand.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2003.marius;
+
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+
+    public ReplaceFirstTextCommand(String target, String replacement) {
+        super(target, replacement);
+    }
+
+    @Override
+    public String execute(String text) {
+        return text.replaceFirst(getTarget(), getReplacement());
+    }
+}
---
+++ src/main/java/edu/ntnu/IDATT2003/marius/command/replace/ReplaceTextCommand.java
@@ -0,0 +1,24 @@
+package edu.ntnu.idatt2003.marius;
+
+public class ReplaceTextCommand implements TextCommand {
+    private final String target;
+    private final String replacement;
+
+    public ReplaceTextCommand(String target, String replacement){
+        this.target = target;
+        this.replacement = replacement;
+    }
+
+    @Override
+    public String execute(String text) {
+        return text.replace(target, replacement);
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public String getReplacement() {
+        return replacement;
+    }
+}
