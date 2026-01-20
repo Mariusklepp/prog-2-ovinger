dd4bd62809bbadf0def0d72f348632de6e1b12e9
Marius Klepp <Mariusklepp@icloud.com>
2026-01-20 15:50:37 +0100

    made selection wrapper

---
+++ src/main/java/edu/ntnu/idatt2003/marius/command/wrap/WrapSelectionTextCommand.java
@@ -0,0 +1,23 @@
+package edu.ntnu.idatt2003.marius.command.wrap;
+
+public class WrapSelectionTextCommand extends WrapTextCommand{
+    String selection;
+
+    public WrapSelectionTextCommand(String opening, String end, String selection) {
+        super(opening, end);
+        this.selection = selection;
+    }
+
+    @Override
+    public String execute(String text) {
+        int position = text.indexOf(selection);
+        String before = text.substring(0, position);
+        String middle = text.substring(position, position + selection.length());
+        String after = text.substring(position + selection.length());
+        return before + getOpening() + middle + getEnd() + after;
+    }
+
+    public String getSelection() {
+        return selection;
+    }
+}
