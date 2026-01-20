5f6b0f96bb9b79ec946379bcb66065f90a439367
Marius Klepp <Mariusklepp@icloud.com>
2026-01-20 14:34:41 +0100

    Fix package folder casing to lowercase idatt2003

---
+++ src/main/java/edu/ntnu/idatt2003/marius/command/wrap/WrapLinesTextCommand.java
@@ -0,0 +1,23 @@
+package edu.ntnu.idatt2003.marius.command.wrap;
+
+public class WrapLinesTextCommand extends WrapTextCommand{
+    String result;
+
+    public WrapLinesTextCommand(String opening, String end) {
+        super(opening, end);
+    }
+
+    @Override
+    public String execute(String text) {
+        String[] lines = text.split("\n");
+        result = "";
+        for (int i = 0; i < lines.length; i++) {
+            result += getOpening() + lines[i] + getEnd();
+            if (i < lines.length - 1) {
+                result += System.lineSeparator();
+
+            }
+        }
+        return  result;
+    }
+}
