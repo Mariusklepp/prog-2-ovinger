d6b9568758d5e5f3ab93371d22e408e2fd103830
Marius Klepp <Mariusklepp@icloud.com>
2026-01-21 11:43:42 +0100

    Wrapselectiontextcommand tester

--- src/test/java/edu/ntnu/idatt2003/marius/WrapSelectionTextCommandTest.java
+++ src/test/java/edu/ntnu/idatt2003/marius/WrapSelectionTextCommandTest.java
@@ -1,4 +1,37 @@
 package edu.ntnu.idatt2003.marius;
 
-public class WrapSelectionTextCommandClass {
+import edu.ntnu.idatt2003.marius.command.wrap.WrapSelectionTextCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class WrapSelectionTextCommandTest {
+    WrapSelectionTextCommand wrapSelectionCommand;
+
+    @BeforeEach
+    void setUp() {
+        wrapSelectionCommand = new WrapSelectionTextCommand("--",
+                                                               "--",
+                                                            "Marius");
+    }
+
+    @Test
+    void executeIsSuccessful() {
+        assertEquals("Hei jeg heter --Marius--",
+                wrapSelectionCommand.execute("Hei jeg heter Marius"));
+    }
+
+    @Test
+    void executeWithoutSelection() {
+        assertEquals("Hei jeg heter Brage",
+                wrapSelectionCommand.execute("Hei jeg heter Brage"));
+    }
+
+    @Test
+    void executeWithBlankSelection() {
+        assertEquals("",
+                wrapSelectionCommand.execute(""));
+    }
 }
+
