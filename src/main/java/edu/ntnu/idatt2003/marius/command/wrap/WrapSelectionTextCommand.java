e86de17b5a5aabaefbb6d28e9c704a877fd67c5f
Marius Klepp <Mariusklepp@icloud.com>
2026-01-21 11:43:14 +0100

    Wrapselectiontextcommand

--- src/main/java/edu/ntnu/idatt2003/marius/command/wrap/WrapSelectionTextCommand.java
+++ src/main/java/edu/ntnu/idatt2003/marius/command/wrap/WrapSelectionTextCommand.java
@@ -11,6 +11,11 @@ public class WrapSelectionTextCommand extends WrapTextCommand{
     @Override
     public String execute(String text) {
         int position = text.indexOf(selection);
+
+        if (position == -1) {
+        return text;
+        }
+
         String before = text.substring(0, position);
         String middle = text.substring(position, position + selection.length());
         String after = text.substring(position + selection.length());
